af2d2fdf0cdc068c62bef4bc022d41eecf6bc0c2
Abeer-Yahya <abeer.yahya799@gmail.com>
2023-04-04 09:16:19 +0300

    edited logger

--- models/logs.js
+++ models/logs.js
@@ -1,15 +1,10 @@
 const mongoose = require("mongoose");
 
-const loggerSchema = mongoose.Schema(
-  {
-    text: {
-      type: String,
-      required: [true, "Add a text"],
-    },
+const loggerSchema = mongoose.Schema({
+  text: {
+    type: String,
+    required: [true, "Add a text"],
   },
-  {
-    timestamps: true,
-  }
-);
+});
 
 module.exports = mongoose.model("Log", loggerSchema);
